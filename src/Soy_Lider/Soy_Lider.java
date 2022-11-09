578342ff3da59a29a95fdd06f49ec1cad224af06
Mario Andres <ArwelWilks1170Q@hotmail.com>
2022-11-09 11:08:53 -0600

    HOLA MUNDO

--- src/Soy_Lider/Soy_Lider.java
+++ src/Soy_Lider/Soy_Lider.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
 public class Soy_Lider extends JFrame {
 
@@ -35,8 +36,12 @@ public class Soy_Lider extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("HOLA MUNDO");
+		lblNewLabel.setBounds(121, 27, 170, 168);
+		contentPane.add(lblNewLabel);
 	}
 
 }
